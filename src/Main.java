d07446fc67c60c404e7370ef1165fafe7663e75c
dhruvvvgg <dg8763@srmist.edu.in>
2026-03-10 09:02:24 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main
+ *
+ * Entry point for the Hotel Booking application.
+ *
+ * @author Dhruv
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Application entry point
+    }
+
+}
